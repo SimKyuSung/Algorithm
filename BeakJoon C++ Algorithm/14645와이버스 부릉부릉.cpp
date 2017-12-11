b78f3eb6b30f70f0db5d91fd23ef9d5bef171fd9
SimKyuSung <sks0706@gmail.com>
2017-12-11 20:06:26 +0900

    Add 14645와이버스 부릉부릉.cpp

---
+++ BeakJoon C++ Algorithm/14645와이버스 부릉부릉.cpp
@@ -0,0 +1,18 @@
+/// 14645와이버스 부릉부릉
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, k, a, b;
+	cin >> n >> k;
+	while (n--) {
+		for (int i = 0; i < k; i++)
+			cin >> a >> b;
+	}
+	cout << "비와이\n";
+}
