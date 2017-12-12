5086e85e71bf0f204f35da0fbb33a5f3f476e249
SimKyuSung <sks0706@gmail.com>
2017-12-12 10:45:45 +0900

    Add 14921용액 합성하기.cpp

---
+++ BeakJoon C++ Algorithm/14921용액 합성하기.cpp
@@ -0,0 +1,27 @@
+/// 14921용액 합성하기
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int a[100001];
+
+int main()
+{
+	int n, ans = 210000000;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+
+	// left, right
+	int l = 0, r = n - 1;
+	while (l < r) {
+		int x = a[l] + a[r];
+		if (x < 0)  l++;
+		else		r--;
+		if (abs(ans) > abs(x))
+			ans = x;
+	}
+	cout << ans << endl;
+}
